4daa8e9fa1f7f92e6c5b1ec6920894ad62f307a7
alexa2401 <a00836348@tec.mx>
2024-10-22 19:06:01 -0600

    corregir suma de los promedios

--- promedio.py
+++ promedio.py
@@ -1,6 +1,9 @@
-for i in range(4):
-	num = input("Introduce un numero: ")
-	sum = sum + num
+suma = 0
 
+for i in range(4):
+	num = float(input("Introduce un numero va: "))
+	sum += num
+
+prom = 0
 prom = sum/4
 print(f"El promedio de tus numeros es: {prom}")
